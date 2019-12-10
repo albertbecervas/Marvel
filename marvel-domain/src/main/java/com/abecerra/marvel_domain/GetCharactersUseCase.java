ee4232643e585ca6ccd953a6753f2e04a2dfe217
Albert Becerra <albertbecerrahervas@gmail.com>
2019-12-10 20:30:29 +0100

    create data domain presentation modules

---
+++ marvel-domain/src/main/java/com/abecerra/marvel_domain/GetCharactersUseCase.java
@@ -0,0 +1,4 @@
+package com.abecerra.marvel_domain;
+
+public class GetCharactersUseCase {
+}
---
+++ marvel-domain/src/test/java/com/abecerra/marvel_domain/GetCharactersUseCaseTest.groovy
@@ -0,0 +1,7 @@
+package com.abecerra.marvel_domain
+
+class GetCharactersUseCaseTest extends GroovyTestCase {
+    void setUp() {
+        super.setUp()
+    }
+}
